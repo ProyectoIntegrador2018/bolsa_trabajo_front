a305e29a0266698b9e82aa77b947eb53cdf64544
Ricardo Lozano <rickyme98@hotmail.com>
2021-04-23 17:32:48 -0500

    Updated admin page to a list to accept users

--- src/components/PanelAdmin/PanelAdmin.tsx
+++ src/components/PanelAdmin/PanelAdmin.tsx
@@ -1,7 +1,82 @@
 import React from 'react';
+import { Button, Col, Jumbotron, Row, Table } from 'reactstrap';
 
 function PanelAdmin() {
-  return (<h1>Panel Admin</h1>);
+
+  const users = [
+    {
+      id: 1,
+      name: "Ricardo Lozano",
+      dateApplied: new Date(Date.now() - 86400000), // that is: 24 * 60 * 60 * 1000
+      age: 70,
+      city: "Monterrey"
+    },
+    {
+      id: 2,
+      name: "Luis Felipe Miranda",
+      dateApplied: new Date(Date.now() - (86400000 * 2)), // that is: 24 * 60 * 60 * 1000
+      age: 70,
+      city: "Santiago"
+    },
+    {
+      id: 3,
+      name: "David Acevedo",
+      dateApplied: new Date(Date.now() - (86400000 * 3)), // that is: 24 * 60 * 60 * 1000
+      age: 70,
+      city: "Mina"
+    },
+  ]
+
+  let applications = [];
+  users.forEach(user => {
+    applications.push(
+
+    )
+  })
+
+  return (
+    <React.Fragment>
+      <Jumbotron>
+        <h1>Autorización de Usuarios</h1>
+      </Jumbotron>
+      <Row>
+        <Col>
+          <h3>Usuarios esperando autorización:</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Edad</th>
+                <th>Municipio</th>
+                <th>Fecha de Aplicación</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {users.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{user.name}</td>
+                    <td>{user.age}</td>
+                    <td>{user.city}</td>
+                    <td>{user.dateApplied.toDateString()}</td>
+                    <td>
+                      <Button color="primary">Ver Usuario</Button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </React.Fragment >
+  );
 };
 
 export default PanelAdmin;
---
+++ src/components/PanelAdmin/RegisterAdmins.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Col, Row } from 'reactstrap';
+
+function RegisterAdmins() {
+    return (
+        <React.Fragment>
+            <Row>
+                <Col>
+                    <h1>Registrar un nuevo administrador</h1>
+                </Col>
+            </Row>
+        </React.Fragment>
+    )
+}
