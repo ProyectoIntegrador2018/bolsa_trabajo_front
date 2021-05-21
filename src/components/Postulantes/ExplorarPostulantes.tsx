c6e471d1a170b39b9ee962954281aca9d521254c
acevedodavid <david@acevedo.com>
2021-05-21 11:01:06 -0500

    API call: Explorar postulantes
    - Bug: can't reload page, stuck loading

--- src/components/Postulantes/ExplorarPostulantes.tsx
+++ src/components/Postulantes/ExplorarPostulantes.tsx
@@ -1,57 +1,134 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import { Table, Row, Col, Button } from 'reactstrap';
 import { Link, useHistory } from 'react-router-dom';
-import listaPostulantes from '../../testing/explorarPostulantes.json'
+import municipios from "../../shared/municipios";
+import { getEmployeesFiltered } from '../../services/employeeService';
+import { useFormik, Formik, Field, useFormikContext } from "formik";
+import { Form, Input, Jumbotron, Label, FormGroup, CustomInput } from "reactstrap";
 
 function ExplorarPostulantes() {
 
-  return (
-    <React.Fragment>
-      <Row className="mx-auto">
-        <Col md={{size: 10, offset: 1}} sm={{size: 12}}>
-          <h1>Explorar Postulantes</h1>
-          <Table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Nombre</th>
-                <th style={{ textAlign: "center" }}>Ver detalle</th>
-              </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <th scope="row">{0}</th>
-              <td>Employee Test</td>
-              <td style={{ textAlign: "center" }}>
-                <Link to={"/postulantes/pZmKDehcEdWE6xR3cU4cdgk7ynp1"}>
-                  <Button color="primary" className="mr-4">
-                    Ver detalle
-                  </Button>
-                </Link>
-              </td>
-            </tr>
-            { listaPostulantes.map((postulante, index) => {
-              let urlDetalle = "/postulantes/" + postulante.id;
-              return (
-              <tr>
-                <th scope="row">{index + 1}</th>
-                <td>{postulante.name}</td>
-                <td style={{ textAlign: "center" }}>
-                  <Link to={urlDetalle}>
-                    <Button color="primary" className="mr-4">
-                      Ver detalle
-                    </Button>
-                  </Link>
-                </td>
-              </tr>
-              )
-            })}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-    </React.Fragment>
-  );
+  const [employees, setEmployees] = useState<any | null>(null);
+
+  //const { handleSubmit } = useFormikContext();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getEmployeesFiltered({field: "municipio", operator: "==", target: "Monterrey"});
+      setEmployees(result);
+    };
+    fetchData();
+  }, []);
+
+  const myHandleChange = (e:any) => {
+    const selectedField = e.target.value;
+    console.log(selectedField);
+  }
+
+
+
+  const formik = useFormik({
+    initialValues: {
+      field: '',
+      operator: '==',
+      target: ''
+    },
+    onSubmit: async (values) => {
+      debugger;
+      const result = await getEmployeesFiltered(values);
+      setEmployees(result);
+    },
+  });
+
+  //const employees = await getEmployeesFiltered();
+
+  if (!employees) {
+    return (
+      <React.Fragment>
+        <Row className="mx-auto">
+          <Col style={{ textAlign: "center" }} md={{size: 12}} sm={{size: 12}}>
+            <div className="spinner-border" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          </Col>
+        </Row>
+      </React.Fragment>);
+  } else {
+    // console.log("Working: ", employees);
+    return (
+      <React.Fragment>
+        <Row className="mx-auto">
+          <Col md={{size: 10, offset: 1}} sm={{size: 12}}>
+            <h1>Explorar Postulantes</h1>
+            <Form onSubmit={formik.handleSubmit}>
+            <Row className="mx-auto">
+              <Col md={{size: 4}} sm={{size: 4}}>
+                <Label htmlFor="field" >Filtrar por:</Label>
+              </Col>
+              <Col md={{size: 4}} sm={{size: 4}}>
+              </Col>
+              <Col md={{size: 4}} sm={{size: 4}}>
+              </Col>
+            </Row>
+            <Row className="mx-auto">
+              <Col md={{size: 4}} sm={{size: 4}}>
+              <FormGroup>
+                <CustomInput type="select" id="field" name="field" onChange={formik.handleChange} value={formik.values.field}>
+                  <option value="municipio">Municipio</option>
+                  <option value="secciones.actividad_deseada.jornada_de_trabajo">Jornada de trabajo</option>
+                  <option value="secciones.clasificacion_puesto.clasificacion">Puesto deseado</option>
+                  <option value="secciones.nivel_de_estudios.nivel_escolar">Nivel de estudios</option>
+                </CustomInput>
+              </FormGroup>
+              </Col>
+              <Col md={{size: 4}} sm={{size: 4}}>
+              <FormGroup>
+                <CustomInput type="select" id="field" name="field" onChange={formik.handleChange} value={formik.values.field}>
+                  {municipios.map((municipio) => {
+                    return <option value={municipio}>{municipio}</option>;
+                  })}
+                </CustomInput>
+              </FormGroup>
+              </Col>
+              <Col md={{size: 4}} sm={{size: 4}}>
+              <FormGroup>
+                  <Button type="submit" value="submit" color="primary" className="mr-4 signbtn">Buscar</Button>
+              </FormGroup>
+              </Col>
+            </Row>
+            </Form>
+            <Table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nombre</th>
+                  <th style={{ textAlign: "center" }}>Ver detalle</th>
+                </tr>
+              </thead>
+              <tbody>
+              { employees.users.map((postulante:any, index:number) => {
+                let urlDetalle = "/postulantes/" + postulante.id;
+                return (
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{postulante.username}</td>
+                  <td style={{ textAlign: "center" }}>
+                    <Link to={urlDetalle}>
+                      <Button color="primary" className="mr-4">
+                        Ver detalle
+                      </Button>
+                    </Link>
+                  </td>
+                </tr>
+                )
+              })}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </React.Fragment>
+    );
+  }
 }
 
 export default ExplorarPostulantes;
---
+++ src/services/employeeService.tsx
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { config } from '../config';
+import { auth } from '../firebase';
+
+export const getEmployeesFiltered = async (body: any) => {
+  const token = await auth.currentUser?.getIdToken();
+  try {
+    const res = await axios.post(
+      config.apiUrl + '/api/user/filter',
+      body,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
