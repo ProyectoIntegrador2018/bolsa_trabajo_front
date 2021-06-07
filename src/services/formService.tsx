68d2417bb9f98c05f0ed16f7937f4442e8c1762f
Luis Felipe Miranda <luisfmiranda97@gmail.com>
2021-06-06 22:06:00 -0500

    post functions

--- src/services/formService.tsx
+++ src/services/formService.tsx
@@ -46,3 +46,47 @@ export const postEmployeeEnrollmentForm = async (enrollmentForm: any) => {
     console.error("Error fetching user", error);
   }
 };
+
+export const getOrganizationForm = async (userId: any) => {
+  if (!userId) return undefined;
+
+  const token = await auth.currentUser?.getIdToken();
+
+  try {
+    const res = await axios.get(config.apiUrl + '/api/user/enrollment-form/' + userId,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log(res.data.enrollmentForm)
+
+      if(res.status == 200) {
+        return res.data.enrollmentForm;
+      }
+      return undefined;
+  }
+  catch (error) {
+    console.error("Error fetching enrollment form", error);
+    return undefined;
+  }
+}
+
+export const postOrganizationEnrollmentForm = async (enrollmentForm: any) => {
+  if (!enrollmentForm) return null;
+  const token = await auth.currentUser?.getIdToken();
+  debugger;
+  try {
+    const res = await axios.post(
+      config.apiUrl + '/api/user/enrollment-form', enrollmentForm, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+    }
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
